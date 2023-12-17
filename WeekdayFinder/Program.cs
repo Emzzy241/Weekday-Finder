a2e29bbfb842db18aa41077ff31e93b6878bb87a
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-17 07:22:02 +0100

    add UI logic

--- WeekdayFinder/Program.cs
+++ WeekdayFinder/Program.cs
@@ -1,45 +1,45 @@
-// using System;
-// using WeekdayFinder.Models;
+using System;
+using WeekdayFinder.Models;
 
-// namespace WeekdayFinder.Models
-// {
-//     public class Program
-//     {
-//         static void Main()
-//         {
-//             Console.WriteLine("Weekday Finder Application");
+namespace WeekdayFinder.Models
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Weekday Finder Application");
 
-//             Console.WriteLine();
+            Console.WriteLine();
            
-//            try
-//            {
-//                 Console.WriteLine("Enter a date in the format MM/DD/YYYY");
-//                 string userDate = Console.ReadLine();
+           try
+           {
+                Console.WriteLine("Enter a date in the format MM/DD/YYYY");
+                string userDate = Console.ReadLine();
                 
-//                 MyWeekdayFinder newWeekdayFinder = new MyWeekdayFinder();
+                MyWeekdayFinder newWeekdayFinder = new MyWeekdayFinder();
 
-//                 string result = newWeekdayFinder.DayOfWeekCalculator(userDate);
-//                 Console.WriteLine();
-//                 Console.WriteLine("--------------------------------------");
-//                 Console.WriteLine($"Your result is: {result}");
-//                 Console.WriteLine("--------------------------------------");
+                string result = newWeekdayFinder.DayOfWeekCalculator(userDate);
+                Console.WriteLine();
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine($"Your result is: {result}");
+                Console.WriteLine("--------------------------------------");
                 
 
 
-//            }
-//            catch
-//            {
-//              Console.WriteLine("Oops!!!, you did not enter in the correct date format of: MM/DD/YYYY");
-//              Console.WriteLine();
-//              Console.WriteLine("Give the app one more shot");
-//              Console.WriteLine();
-//              Console.WriteLine();
-//              Main();
+           }
+           catch
+           {
+             Console.WriteLine("Oops!!!, you did not enter in the correct date format of: MM/DD/YYYY");
+             Console.WriteLine();
+             Console.WriteLine("Give the app one more shot");
+             Console.WriteLine();
+             Console.WriteLine();
+             Main();
 
-//            }
+           }
 
             
             
-//         }        
-//     }
-// }
+        }        
+    }
+}
